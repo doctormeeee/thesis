c91398ee7a69e959aefacaf6ba7aad67ab5b9bd8
Jiawei Deng <470727102doc@gmail.com>
2023-10-15 12:05:50 +1100

    backup

--- Performance_Profiling/ResNet50/resnet50Test.py
+++ Performance_Profiling/ResNet50/resnet50Test.py
@@ -4,10 +4,12 @@ from torchvision import datasets, transforms
 # from torchvision.transforms import ToPILImage
 from torchvision.models.resnet import resnet50
 from tqdm import tqdm
+import time
 
-def load_data(data_dir='imagenet', batch_size=''):
+def load_data(data_dir='', batch_size=''):
 
-	valdir = data_dir + '/val'
+	# valdir = data_dir + '/val'
+	valdir = data_dir
 	normalize = transforms.Normalize(
 		mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 	size = 224
@@ -76,11 +78,16 @@ def evaluate(model, val_loader, loss_fn):
 	Loss = 0
 	for iteraction, (images, labels) in tqdm(
 		enumerate(val_loader), total=len(val_loader)):
+		# images.
 		images = images.to(device)
 		labels = labels.to(device)
 		#pdb.set_trace()
 		outputs = model(images)
 		loss = loss_fn(outputs, labels)
+		# print(torch.argmax(outputs, axis=1))
+		# print(outputs.size())
+		# print(torch.max(outputs, axis=1)[1])
+		# print(labels)
 		Loss += loss.item()
 		total += images.size(0)
 		acc1, acc5 = accuracy(outputs, labels, topk=(1, 5))
@@ -108,9 +115,19 @@ if __name__ == "__main__":
 
 	loss_fn = torch.nn.CrossEntropyLoss().to(device)
 
-	val_loader = load_data(data_dir='imagenet', batch_size=10)
+	# val_loader = load_data(data_dir='imagenet', batch_size=10)
+	val_loader = load_data(data_dir='img', batch_size=10)
+
+	start_time = time.time()
+ 
 
 	acc1_gen, acc5_gen, loss_gen = evaluate(model, val_loader, loss_fn)
+ 
+	end_time = time.time()
+ 
+	execution_time = end_time - start_time
+ 
+	print(f'Execution time: {execution_time}')
 
 	# logging accuracy
 	print('loss: %g' % (loss_gen))
