0dcc975fcaef79f7c0fe35cb99dd5fd1a45e69fc
Jiawei Deng <470727102doc@gmail.com>
2023-11-22 19:13:51 +1100

    push inspector

---
+++ Inspector/inspect_modified/ModifiedToyModel.py
@@ -0,0 +1,29 @@
+# GENETARED BY NNDCT, DO NOT EDIT!
+
+import torch
+from torch import tensor
+import pytorch_nndct as py_nndct
+
+class ModifiedToyModel(py_nndct.nn.NndctQuantModel):
+    def __init__(self):
+        super(ModifiedToyModel, self).__init__()
+        self.module_0 = py_nndct.nn.Input() #ModifiedToyModel::input_0(ModifiedToyModel::nndct_input_0)
+        self.module_1 = py_nndct.nn.Interpolate() #ModifiedToyModel::ModifiedToyModel/Upsample[upsample]/ret.3(ModifiedToyModel::nndct_resize_1)
+        self.module_2 = py_nndct.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=[3, 3], stride=[1, 1], padding=[1, 1], dilation=[1, 1], groups=1, bias=True) #ModifiedToyModel::ModifiedToyModel/Conv2d[conv]/ret.5(ModifiedToyModel::nndct_conv2d_2)
+        self.module_3 = py_nndct.nn.ReLU(inplace=False) #ModifiedToyModel::ModifiedToyModel/ReLU[relu]/ret.7(ModifiedToyModel::nndct_relu_3)
+        self.module_4 = py_nndct.nn.AdaptiveAvgPool2d(output_size=[2, 2]) #ModifiedToyModel::ModifiedToyModel/AdaptiveAvgPool2d[adaptive_avg_pool2d]/230(ModifiedToyModel::nndct_adaptive_avg_pool2d_4)
+        self.module_5 = py_nndct.nn.Module('nndct_permute') #ModifiedToyModel::ModifiedToyModel/ret.9(ModifiedToyModel::nndct_permute_5)
+        self.module_6 = py_nndct.nn.Module('nndct_shape') #ModifiedToyModel::ModifiedToyModel/240(ModifiedToyModel::nndct_shape_6)
+        self.module_7 = py_nndct.nn.Module('nndct_reshape') #ModifiedToyModel::ModifiedToyModel/ret(ModifiedToyModel::nndct_reshape_7)
+
+    @py_nndct.nn.forward_processor
+    def forward(self, *args):
+        output_module_0 = self.module_0(input=args[0])
+        output_module_0 = self.module_1(input=output_module_0, size=None, scale_factor=[2.0,2.0], mode='nearest')
+        output_module_0 = self.module_2(output_module_0)
+        output_module_0 = self.module_3(output_module_0)
+        output_module_0 = self.module_4(output_module_0)
+        output_module_0 = self.module_5(dims=[0,2,3,1], input=output_module_0)
+        output_module_6 = self.module_6(input=output_module_0, dim=0)
+        output_module_7 = self.module_7(input=output_module_0, shape=[output_module_6,-1])
+        return output_module_7
